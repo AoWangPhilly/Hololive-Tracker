00b8b6adb4b012d5bd1c9bfe4d04b35443b93ef6
Ao Wang <aw3338@drexel.edu>
2022-09-08 14:37:49 -0400

    move client to separate module

---
+++ src/twitter_pipeline/hololive_stream.py
@@ -0,0 +1,42 @@
+import json
+from pprint import pprint
+
+from tweepy import StreamingClient, StreamRule
+
+from constants import EN_MYTH_TWITTER_ID, EN_VSINGER_TWITTER_ID, EN_COUNCIL_TWITTER_ID, EN_TEMPUS_TWITTER_ID
+
+
+def create_rule() -> StreamRule:
+    twitter_ids = EN_MYTH_TWITTER_ID + EN_VSINGER_TWITTER_ID + EN_COUNCIL_TWITTER_ID + EN_TEMPUS_TWITTER_ID
+    rule_value = " OR ".join([f"from:{twitter_id}" for twitter_id in twitter_ids])
+    return StreamRule(value=rule_value)
+
+
+def delete_rule(client: StreamingClient) -> None:
+    if rule_data := client.get_rules().data:
+        current_rules = list(map(lambda rule: rule.id, rule_data))
+        client.delete_rules(current_rules)
+
+
+def add_rule(client: StreamingClient) -> None:
+    rules = create_rule()
+    client.add_rules(rules)
+
+
+class HololiveStreamingClient(StreamingClient):
+    def on_connect(self) -> None:
+        print("Stream connected! :)")
+
+    def on_data(self, raw_data) -> None:
+        pprint(json.loads(raw_data))
+        print("\n")
+
+    def on_exception(self, exception: Exception) -> None:
+        print("An exception has occurred")
+        print(f"{exception=}")
+
+    def on_connection_error(self) -> None:
+        print("A streaming connection has occurred! Or stream has timed out!")
+
+    def on_disconnect(self) -> None:
+        print("Stream disconnected! Good bye!")
