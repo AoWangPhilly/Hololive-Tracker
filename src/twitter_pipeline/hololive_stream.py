7a9fa7b0266090c53de257526decf1694b64f738
Ao Wang <aw3338@drexel.edu>
2022-09-08 22:57:29 -0400

    refactor

--- src/twitter_pipeline/hololive_stream.py
+++ src/twitter_pipeline/hololive_stream.py
@@ -6,25 +6,25 @@ from tweepy import StreamingClient, StreamRule
 from constants import EN_MYTH_TWITTER_ID, EN_VSINGER_TWITTER_ID, EN_COUNCIL_TWITTER_ID, EN_TEMPUS_TWITTER_ID
 
 
-def create_rule() -> StreamRule:
+def create_rules() -> StreamRule:
     twitter_ids = EN_MYTH_TWITTER_ID + EN_VSINGER_TWITTER_ID + EN_COUNCIL_TWITTER_ID + EN_TEMPUS_TWITTER_ID
     rule_value = " OR ".join([f"from:{twitter_id}" for twitter_id in twitter_ids])
     return StreamRule(value=rule_value)
 
 
-def delete_rule(client: StreamingClient) -> None:
-    if rule_data := client.get_rules().data:
-        current_rules = list(map(lambda rule: rule.id, rule_data))
-        client.delete_rules(current_rules)
-
-
-def add_rule(client: StreamingClient) -> None:
-    rules = create_rule()
-    client.add_rules(rules)
-
-
 class HololiveStreamingClient(StreamingClient):
+
+    def _add_rule(self) -> None:
+        rules = create_rules()
+        self.add_rules(add=rules)
+
+    def _delete_rule(self) -> None:
+        if rule_data := self.get_rules().data:
+            current_rules = list(map(lambda rule: rule.id, rule_data))
+            self.delete_rules(ids=current_rules)
+
     def on_connect(self) -> None:
+        self._add_rule()
         print("Stream connected! :)")
 
     def on_data(self, raw_data) -> None:
@@ -39,4 +39,5 @@ class HololiveStreamingClient(StreamingClient):
         print("A streaming connection has occurred! Or stream has timed out!")
 
     def on_disconnect(self) -> None:
+        self._delete_rule()
         print("Stream disconnected! Good bye!")
--- src/twitter_pipeline/main.py
+++ src/twitter_pipeline/main.py
@@ -1,16 +1,13 @@
 import os
 
 from constants import TWEET_FIELDS, EXPANSIONS, MEDIA_FIELDS, POLL_FIELDS, USER_FIELDS, PLACE_FIELDS
-from hololive_stream import HololiveStreamingClient, delete_rule, add_rule
+from hololive_stream import HololiveStreamingClient
 
 BEARER_TOKEN = os.environ.get("bearer_token")
 
 
 def main():
     client = HololiveStreamingClient(bearer_token=BEARER_TOKEN)
-    delete_rule(client)
-    add_rule(client)
-
     client.filter(tweet_fields=TWEET_FIELDS, expansions=EXPANSIONS, media_fields=MEDIA_FIELDS,
                   poll_fields=POLL_FIELDS, user_fields=USER_FIELDS, place_fields=PLACE_FIELDS)
 
