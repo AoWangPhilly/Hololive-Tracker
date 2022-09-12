c0c078ab3ad3b37f3b3e5715e4d1c6f74133c388
AoWangPhilly <aw3338@drexel.edu>
2022-09-12 15:59:39 -0400

    use python-decouple

--- src/twitter_pipeline/database.py
+++ src/twitter_pipeline/database.py
@@ -1,16 +1,16 @@
-import os
 from typing import Dict
 
+from decouple import config
 from sqlalchemy import MetaData, Table, Column, Integer, DateTime, JSON
 from sqlalchemy import create_engine
 from sqlalchemy.sql import func
 from sqlalchemy_utils import database_exists, create_database
 
-DB_USERNAME = os.environ.get("db_username")
-DB_PASSWORD = os.environ.get("db_password")
-DB_HOSTNAME = os.environ.get("db_hostname")
-DB_PORT = os.environ.get("db_port")
-DB_NAME = os.environ.get("db_name")
+DB_USERNAME = config("db_username")
+DB_PASSWORD = config("db_password")
+DB_HOSTNAME = config("db_hostname")
+DB_PORT = config("db_port")
+DB_NAME = config("db_name")
 
 SQLALCHEMY_DB_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}"
 
