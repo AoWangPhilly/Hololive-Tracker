bd501601e6df98f734408de07b5c647cf5b94476
Ao Wang <aw3338@drexel.edu>
2022-12-09 15:39:46 -0500

    use session instead

--- src/twitter/user.py
+++ src/twitter/user.py
@@ -1,13 +1,12 @@
 # https://api.twitter.com/2/users?ids=1283653858510598144&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld
 
-from typing import List, Union
+from typing import List, Union, Dict
 
 import requests
 from decouple import config
-from sqlalchemy import insert
 
 from constants import USER_FIELDS, EN_TWITTER_ID
-from database import engine
+from database import Session
 from models import RawMetric
 
 BASE_URL = "https://api.twitter.com/2/users"
@@ -22,7 +21,7 @@ class User:
         user_fields = ",".join(USER_FIELDS)
         return f"{BASE_URL}?ids={ids}&user.fields={user_fields}"
 
-    def call_endpoint(self):
+    def call_endpoint(self) -> Dict:
         url = self._build_url()
         response = requests.get(url, headers={"Authorization": f'Bearer {config("bearer_token")}'})
         if not response.ok:
@@ -31,14 +30,15 @@ class User:
             )
         return response.json()
 
-    def save_to_db(self):
+    def save_to_db(self) -> None:
         data = self.call_endpoint()
-        query = insert(RawMetric).values(data=data)
-        conn = engine.connect()
+        raw_metric = RawMetric(data=data)
 
-        conn.execute(query)
+        session = Session()
+        session.add(raw_metric)
+        session.commit()
 
 
 if __name__ == "__main__":
     users = User(ids=EN_TWITTER_ID)
-    print(users.save_to_db())
+    users.save_to_db()
