89de6285831addf6315f4e333f44b15906dbb373
Ao Wang <aw3338@drexel.edu>
2022-12-10 13:04:45 -0500

    add in youtube channel ids

---
+++ src/constants.py
@@ -0,0 +1,66 @@
+USER_FIELDS = [
+    "created_at", "description", "entities",
+    "id", "location", "name", "pinned_tweet_id",
+    "profile_image_url", "protected", "public_metrics",
+    "url", "username", "verified", "withheld"
+]
+
+EN_MYTH_TWITTER_ID = [
+    1283653858510598144,  # mori calliope
+    1283646922406760448,  # takanashi kiara
+    1283650008835743744,  # ninomae inanis
+    1283657064410017793,  # gawr gura
+    1283656034305769472  # watson amelia
+]
+
+EN_VSINGER_TWITTER_ID = [1363705980261855232]  # IRyS
+
+EN_COUNCIL_TWITTER_ID = [
+    1409819816194576394,  # tsukumo sana
+    1409784760805650436,  # ceres fauna
+    1409817096523968513,  # ouro kronii
+    1409817941705515015,  # nanashi mumei
+    1409783149211443200  # hakos baelz
+]
+
+EN_TEMPUS_TWITTER_ID = [
+    1536575088996524032,  # regis altare
+    1536576325296996352,  # magni dezmond
+    1536577295632441344,  # axel syrios
+    1536579341332516864  # noir vesper
+]
+
+EN_TWITTER_ID = EN_MYTH_TWITTER_ID + \
+                EN_VSINGER_TWITTER_ID + \
+                EN_COUNCIL_TWITTER_ID + \
+                EN_TEMPUS_TWITTER_ID
+
+EN_MYTH_YOUTUBE_CHANNEL_ID = [
+    "UCyl1z3jo3XHR1riLFKG5UAg",  # Watson Amelia
+    "UCHsx4Hqa-1ORjQTh9TYDhww",  # Takanashi Kiara
+    "UCL_qhgtOy0dy1Agp8vkySQg",  # Mori Calliope
+    "UCoSrY_IQQVpmIRZ9Xf-y93g"  # Gawr Gura
+    "UCMwGHR0BTZuLsmjY_NT5Pwg",  # Ninomae Ina'nis
+]
+
+EN_VSINGER_YOUTUBE_CHANNEL_ID = ["UC8rcEBzJSleTkf_-agPM20g"]  # IRyS
+
+EN_COUNCIL_YOUTUBE_CHANNEL_ID = [
+    "UC3n5uGu18FoCy23ggWWp8tA",  # Nanashi Mumei
+    "UCmbs8T6MWqUHP1tIQvSgKrg",  # Ouro Kronii
+    "UCO_aKKYxn4tvrqPjcTzZ6EQ",  # Ceres Fauna
+    "UCgmPnx-EEeOrZSg5Tiw7ZRQ",  # Hakos Baelz
+    "UCsUj0dszADCGbF3gNrQEuSQ",  # Tsukumo Sana
+]
+
+EN_TEMPUS_YOUTUBE_CHANNEL_ID = [
+    "UC2hx0xVkMoHGWijwr_lA01w",  # Axel Syrios
+    "UCDRWSO281bIHYVi-OV3iFYA",  # Noir Vesper
+    "UC7MMNHR-kf9EN1rXiesMTMw",  # Magni Dezmond
+    "UCyxtGMdWlURZ30WSnEjDOQw",  # Regis Altare
+]
+
+EN_YOUTUBE_CHANNEL_ID = EN_MYTH_YOUTUBE_CHANNEL_ID + \
+                        EN_VSINGER_YOUTUBE_CHANNEL_ID + \
+                        EN_COUNCIL_YOUTUBE_CHANNEL_ID + \
+                        EN_TEMPUS_YOUTUBE_CHANNEL_ID
