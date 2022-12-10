7f599dc65e3e86b3d77e9d9f7303cb4e3f891388
Ao Wang <aw3338@drexel.edu>
2022-12-10 16:56:02 -0500

    add yt metrics to cronjob

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
-import src.twitter.cleanser as cleanser
-from src.constants import EN_TWITTER_ID
-from src.database import Session
-from src.models import RawTwitterMetric
+import src.models as models
+import src.twitter.cleanser as twitter_cleanser
+import src.youtube.cleanser as youtube_cleanser
+from src.constants import EN_TWITTER_ID, EN_YOUTUBE_CHANNEL_ID
+from src.database import Session, engine, setup_db
 from src.twitter.user import User
+from src.youtube.channel import Channel
 
 
 def main() -> None:
-    users = User(ids=EN_TWITTER_ID)
-    users.save_to_db()
+    setup_db()
+    models.Base.metadata.create_all(engine)
+
     session = Session()
-    last_record = session.query(RawTwitterMetric).order_by(RawTwitterMetric.id.desc()).first()
-    cleanser.save_to_db([last_record])
+
+    users = User(ids=EN_TWITTER_ID)
+    users.save_to_db(models.RawTwitterMetric)
+    last_record = session.query(models.RawTwitterMetric).order_by(models.RawTwitterMetric.id.desc()).first()
+    twitter_cleanser.save_to_db([last_record])
+
+    channel = Channel(channel_id="")
+    for channel_id in EN_YOUTUBE_CHANNEL_ID:
+        channel.set_channel_id(channel_id=channel_id)
+        channel.save_to_db(models.RawYouTubeMetric)
+        last_record = session.query(models.RawYouTubeMetric).order_by(models.RawYouTubeMetric.id.desc()).first()
+        youtube_cleanser.save_to_db(last_record)
 
 
 if __name__ == "__main__":
@@ -18,3 +31,4 @@ if __name__ == "__main__":
 
 #  sudo crontab -u aowang -e\l
 #  sudo service cron restart
+#  cd "/mnt/c/Users/Ao Wang/projects/Hololive-Tracker" && /home/aowang/.virtualenvs/Hololive-Tracker/bin/python3 -m src.main
